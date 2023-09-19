3b1763e97f8e5c04faaf1c9573dd0fe97af11845
harleigh <harleigh.marsh@gmail.com>
2023-09-19 10:10:09 -0700

    initial commit: Component Structure Made

---
+++ README.md
@@ -0,0 +1,3 @@
+A basic collection of various ways to obtain and process input from the user;
+
+One of the more neat elements of this basic project, is a text field that only takes floating numbers and rejects any keystrokes that do not form a floating number.
---
+++ src/Components/StepApp.js
@@ -0,0 +1,14 @@
+import { useState } from "react"
+
+export default function  StepApp() {
+    const [currentStep, setCurrentStep] = useState(0);
+
+
+    return (
+        <>
+        <div className="stepsApp">
+            
+        </div>
+        </>
+    )
+}
