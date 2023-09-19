ade04c6ace40d0ed94047b60321a0ff24976fb7b
harleigh <harleigh.marsh@gmail.com>
2023-09-19 13:32:16 -0700

    current step and all previous steps are colored as active

--- src/Components/Numbers.js
+++ src/Components/Numbers.js
@@ -2,10 +2,10 @@ import { messages } from "../messageData"
 
 export function Numbers ({currentStep}) {
 
-    //the current step will always be "active"
+    //the current step and all previous will always be "active"
     const generateSteps = () => {
         return messages.map( (_, idx) => {
-            const classType = (idx === currentStep? "active":"")
+            const classType = (idx <= currentStep? "active":"")
             return ( <div className={classType}
                           key={crypto.randomUUID()}>
                         {idx+1}
